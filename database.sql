277dbce5a1ed3080af6f359866ef575bb20c3332
Kirtan Ugreja <ugrejakirtan0@gmail.com>
2025-07-12 13:25:02 +0530

    Update database.sql
    updated the message table

--- database.sql
+++ database.sql
@@ -70,12 +70,11 @@ INSERT INTO skills (user_id, name, description, category, level, is_offered) VAL
 (3, 'Web Design', 'Want to learn modern web design', 'Technology', 'beginner', FALSE);
 
 CREATE TABLE IF NOT EXISTS messages (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        sender_id VARCHAR(50) NOT NULL,
-        receiver_id VARCHAR(50) NOT NULL,
-        message_text TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (sender_id) REFERENCES users(id) ON DELETE CASCADE,
-        FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
-    )
-
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    message_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
+);
